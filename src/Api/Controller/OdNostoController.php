b01c544c7726528a269c4723152b47759f0dba35
Vladislav Borisenko <vladislav.borisenko@amasty.com>
2022-02-02 14:08:15 +0300

    Some js fixes.

--- src/Api/Controller/OdNostoController.php
+++ src/Api/Controller/OdNostoController.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Od\NostoIntegration\Api\Controller;
 
-use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Shopware\Core\Framework\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,8 +17,8 @@ class OdNostoController extends AbstractController
     /**
      * @RouteScope(scopes={"api"})
      * @Route(
-     *     "/api/_action/od-nosto/index",
-     *     name="api.action.od-nosto.index",
+     *     "/api/_action/od-nosto/schedule-full-product-sync",
+     *     name="api.action.od-nosto.schedule.full.product.sync",
      *     methods={"POST"}
      * )
      * @param Request $request
