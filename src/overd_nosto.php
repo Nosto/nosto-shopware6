d5e4812a41ca1f94a81e420287d3308b21ae8a86
Galust Betkhemyan <g.betkhemyan@signundsinn.de>
2022-11-15 12:11:27 +0400

    Added compatibility with last version [https://www.wrike.com/open.htm?id=1003640638]

--- src/overd_nosto.php
+++ src/overd_nosto.php
@@ -4,6 +4,7 @@ namespace Od\NostoIntegration;
 
 use Composer\Autoload\ClassLoader;
 use Doctrine\DBAL\Connection;
+use Od\NostoIntegration\Utils\Loader\FlexibleXmlFileLoader;
 use Od\NostoIntegration\Utils\MigrationHelper;
 use Od\Scheduler\OdScheduler;
 use Shopware\Core\Framework\Parameter\AdditionalBundleParameters;
@@ -11,6 +12,10 @@ use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Shopware\Core\Framework\Plugin\Util\AssetService;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class overd_nosto extends Plugin
 {
@@ -69,5 +74,25 @@ class overd_nosto extends Plugin
 
         $classLoader->unregister();
         $classLoader->register(false);
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        $this->registerContainerFile($container);
+        parent::build($container);
+    }
+
+    private function registerContainerFile(ContainerBuilder $container): void
+    {
+        $fileLocator = new FileLocator($this->getPath());
+        $loaderResolver = new LoaderResolver([
+            new FlexibleXmlFileLoader($container, $fileLocator),
+        ]);
+        $delegatingLoader = new DelegatingLoader($loaderResolver);
+
+        $path = $this->getPath().'/Resources/config/flexible_services.xml';
+        if (file_exists($path)) {
+            $delegatingLoader->load($path);
+        }
     }
 }
