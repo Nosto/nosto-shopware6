bfbb219ad0b35228f2891833777e67c5685e2b6b
roman_h <roman.h@overdose.digital>
2023-05-11 10:06:29 +0300

    adjusted restore card functionality

--- src/Migration/Migration1683746554CreateTable.php
+++ src/Migration/Migration1683746554CreateTable.php
@@ -5,11 +5,11 @@ namespace Od\NostoIntegration\Migration;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1683544499RestoreCartTable extends MigrationStep
+class Migration1683746554CreateTable extends MigrationStep
 {
     public function getCreationTimestamp(): int
     {
-        return 1683544499;
+        return 1683746554;
     }
 
     public function update(Connection $connection): void
@@ -26,6 +26,7 @@ class Migration1683544499RestoreCartTable extends MigrationStep
         SQL;
         $connection->executeStatement($sql);
     }
+
     public function updateDestructive(Connection $connection): void
     {
         // implement update destructive
