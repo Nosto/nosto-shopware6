5eb44cb0fbaf85281eaab1ada4c80cf1ed23665f
Tobias Graml <tobias.graml@nosto.com>
2024-02-12 15:41:55 +0100

    Replace cookie after storefront request

--- src/Subscriber/FrontendSubscriber.php
+++ src/Subscriber/FrontendSubscriber.php
@@ -6,8 +6,12 @@ namespace Nosto\NostoIntegration\Subscriber;
 
 use Nosto\NostoIntegration\Model\ConfigProvider;
 use Nosto\NostoIntegration\Struct\Config;
+use Shopware\Storefront\Framework\Routing\StorefrontResponse;
 use Shopware\Storefront\Pagelet\Header\HeaderPageletLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class FrontendSubscriber implements EventSubscriberInterface
 {
@@ -23,6 +27,7 @@ class FrontendSubscriber implements EventSubscriberInterface
     {
         return [
             HeaderPageletLoadedEvent::class => 'onHeaderLoaded',
+            KernelEvents::RESPONSE => 'onKernelResponse',
         ];
     }
 
@@ -34,5 +39,27 @@ class FrontendSubscriber implements EventSubscriberInterface
         );
         $nostoConfig = new Config($config);
         $event->getContext()->addExtension('nostoConfig', $nostoConfig);
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        if (!($event->getResponse() instanceof StorefrontResponse)) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $request = $event->getRequest();
+
+        if ($request->cookies->has('od-nosto-track-allow')) {
+            $cookie = Cookie::create('od-nosto-track-allow', '1', strtotime('-1 day'))
+                ->withHttpOnly(false);
+            $cookie->setSecureDefault($request->isSecure());
+            $response->headers->setCookie($cookie);
+
+            $cookie = Cookie::create('nosto-integration-track-allow', '1', strtotime('+30 days'))
+                ->withHttpOnly(false);
+            $cookie->setSecureDefault($request->isSecure());
+            $response->headers->setCookie($cookie);
+        }
     }
 }
