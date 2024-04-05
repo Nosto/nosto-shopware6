9af631b3b3b59c0ce8559c302732faae078222d8
Tobias Graml <tobias.graml@nosto.com>
2024-04-05 11:39:47 +0200

    Adapt storefront

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,16 +1,8 @@
-import NostoPlugin from './js/plugin/nosto.plugin'
-import NostoConfiguration from './js/plugin/nosto-configuration.plugin';
-import NostoSearchSessionParams from './js/plugin/nosto-search-session-params';
-import NostoFilterRange from './js/plugin/listing/filter-range.plugin';
-import NostoFilterPropertySelectPlugin from './js/plugin/listing/filter-property-select.plugin';
-
-// Register plugins via the existing PluginManager
-const PluginManager = window.PluginManager;
-PluginManager.register('NostoPlugin', NostoPlugin, '[data-nosto-cart-plugin]');
-PluginManager.register('NostoConfiguration', NostoConfiguration, '[data-nosto-configuration]');
-PluginManager.register('NostoSearchSessionParams', NostoSearchSessionParams, '[data-nosto-search-session-params]');
-PluginManager.override('FilterRange', NostoFilterRange, '[data-filter-range]');
-PluginManager.override('FilterPropertySelect', NostoFilterPropertySelectPlugin, '[data-filter-property-select]');
+window.PluginManager.register('NostoPlugin', () => import('./js/plugin/nosto.plugin'), '[data-nosto-cart-plugin]');
+window.PluginManager.register('NostoConfiguration', () => import('./js/plugin/nosto-configuration.plugin'), '[data-nosto-configuration]');
+window.PluginManager.register('NostoSearchSessionParams', () => import('./js/plugin/nosto-search-session-params'), '[data-nosto-search-session-params]');
+window.PluginManager.override('FilterRange', () => import('./js/plugin/listing/filter-range.plugin'), '[data-filter-range]');
+window.PluginManager.override('FilterPropertySelect', () => import('./js/plugin/listing/filter-property-select.plugin'), '[data-filter-property-select]');
 
 if (module.hot) {
     module.hot.accept();
