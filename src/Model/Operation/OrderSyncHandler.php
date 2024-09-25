269b03f4383d525201096003549d1a7c6181e841
Radomir Butacevic <radomir.butacevic@soprex.com>
2024-09-25 14:09:02 +0200

    Correct the changes

--- src/Model/Operation/OrderSyncHandler.php
+++ src/Model/Operation/OrderSyncHandler.php
@@ -18,11 +18,11 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\{EntityCollection, EntityRepository, Search\Filter\EqualsFilter};
+use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SalesChannel\Context\AbstractSalesChannelContextFactory;
 use Shopware\Core\System\SalesChannel\Context\SalesChannelContextService;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Throwable;
-use Shopware\Core\Framework\Uuid\Uuid;
 
 class OrderSyncHandler implements JobHandlerInterface
 {
