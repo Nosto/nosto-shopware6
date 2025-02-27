ddaf73ce2c88670f7be878e623060da7c80898a6
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-02-27 12:12:08 +0100

    Correct the changes

--- src/Model/Operation/ProductSyncHandler.php
+++ src/Model/Operation/ProductSyncHandler.php
@@ -245,7 +245,7 @@ class ProductSyncHandler implements Job\JobHandlerInterface
         } elseif (count($configuratorGroups)) {
             $mainProducts->merge($this->handleConfiguratorGroups($product));
         } elseif (!$product->getActive()) {
-            if ($variant = $this->handleFirstActiveVariant($product, $context)) {
+            if ($variant = $this->handleFirstActiveVariant($product)) {
                 $mainProducts->add($variant);
             }
         }
