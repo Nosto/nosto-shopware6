07c52af84032b2e6961ae8018bfa04cf5a685daf
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-03-04 15:17:11 +0100

    Add fix

--- src/Model/Operation/ProductSyncHandler.php
+++ src/Model/Operation/ProductSyncHandler.php
@@ -351,7 +351,7 @@ class ProductSyncHandler implements Job\JobHandlerInterface
             }
         }
 
-        if ($mainProduct) {
+        if ($mainProduct && $mainProduct->getId() === $variantConfig->getMainVariantId()) {
             $mainProduct->setChildren($variants);
         }
 
