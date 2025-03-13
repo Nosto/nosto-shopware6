516d865a86de794362b3f0293a13f0717a2c0eb2
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2025-03-13 17:20:41 +0200

    no cookie, no analytics tracking. Simply check if not there dont send data

--- src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
+++ src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
@@ -136,6 +136,9 @@ class ProductSearchRoute extends AbstractProductSearchRoute
             );
             //php changes the . to _ in the cookie
             $sessionId = $request->cookies->get("2c_cId");
+            if (!$sessionId){
+                return;
+            }
             $userAgent = $request->headers->get('User-Agent');
             $tracker = new AnalyticsSearchTracking($merchantId, $sessionId, $userAgent);
             $page = $productListing->getPage();
