969a65178d66ba9c8b0477a53ad091baa191d305
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-03-25 16:08:03 +0100

    Add info help text and remove Nosto sorting if service inactive

---
+++ src/Decorator/Core/Content/Product/SalesChannel/Search/ResolvedCriteriaProductSearchRoute.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Nosto\NostoIntegration\Decorator\Core\Content\Product\SalesChannel\Search;
+
+use Nosto\NostoIntegration\Search\Request\Handler\SortHandlers\RecommendationSortingHandler;
+use Shopware\Core\Content\Product\Events\ProductSearchCriteriaEvent;
+use Shopware\Core\Content\Product\Events\ProductSearchResultEvent;
+use Shopware\Core\Content\Product\ProductEvents;
+use Shopware\Core\Content\Product\SalesChannel\Listing\Processor\CompositeListingProcessor;
+use Shopware\Core\Content\Product\SalesChannel\Search\AbstractProductSearchRoute;
+use Shopware\Core\Content\Product\SalesChannel\Search\ProductSearchRouteResponse;
+use Shopware\Core\Framework\DataAbstractionLayer\DefinitionInstanceRegistry;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\RequestCriteriaBuilder;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class ResolvedCriteriaProductSearchRoute extends AbstractProductSearchRoute
+{
+    final public const DEFAULT_SEARCH_SORT = 'score';
+    final public const STATE = 'search-route-context';
+
+    /**
+     * @internal
+     */
+    public function __construct(
+        private readonly AbstractProductSearchRoute $decorated,
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly DefinitionInstanceRegistry $registry,
+        private readonly RequestCriteriaBuilder $criteriaBuilder,
+        private readonly CompositeListingProcessor $processor
+    ) {
+    }
+
+    public function getDecorated(): AbstractProductSearchRoute
+    {
+        return $this->decorated;
+    }
+
+    #[Route(path: '/store-api/search', name: 'store-api.search', methods: ['POST'], defaults: ['_entity' => 'product'])]
+    public function load(Request $request, SalesChannelContext $context, Criteria $criteria): ProductSearchRouteResponse
+    {
+        $criteria->addState(self::STATE);
+
+        $criteria = $this->criteriaBuilder->handleRequest(
+            $request,
+            $criteria,
+            $this->registry->getByEntityName('product'),
+            $context->getContext()
+        );
+
+        // will be handled via processor in next line
+        $criteria->setLimit(null);
+
+        $this->processor->prepare($request, $criteria, $context);
+
+        $this->eventDispatcher->dispatch(
+            new ProductSearchCriteriaEvent($request, $criteria, $context),
+            ProductEvents::PRODUCT_SEARCH_CRITERIA
+        );
+
+        $response = $this->getDecorated()->load($request, $context, $criteria);
+
+        $this->processor->process($request, $response->getListingResult(), $context);
+
+        $this->eventDispatcher->dispatch(
+            new ProductSearchResultEvent($request, $response->getListingResult(), $context),
+            ProductEvents::PRODUCT_SEARCH_RESULT
+        );
+
+        $response->getListingResult()->getAvailableSortings()->removeByKey(
+            RecommendationSortingHandler::MERCHANDISING_SORTING_KEY
+        );
+
+        $response->getListingResult()->addCurrentFilter('search', $request->get('search'));
+
+        return $response;
+    }
+}
