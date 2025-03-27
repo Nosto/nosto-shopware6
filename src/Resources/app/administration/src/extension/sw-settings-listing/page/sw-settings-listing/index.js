3f85112441e742623340115a318cf8b773d14f58
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-03-27 10:32:15 +0100

    Adapt ESLint test

--- src/Resources/app/administration/src/extension/sw-settings-listing/page/sw-settings-listing/index.js
+++ src/Resources/app/administration/src/extension/sw-settings-listing/page/sw-settings-listing/index.js
@@ -5,18 +5,24 @@ Shopware.Component.override('sw-settings-listing', {
 
     computed: {
         version() {
-            const currentVersion = Shopware.Context.app.config.version
-            const compareVersion = '6.6.9'
+            const currentVersion = Shopware.Context.app.config.version;
+            const compareVersion = '6.6.9';
 
-            const oldParts = compareVersion.split('.')
-            const newParts = currentVersion.split('.')
-            for (var i = 0; i < newParts.length; i++) {
-                const a = ~~newParts[i]
-                const b = ~~oldParts[i]
-                if (a > b) return true
-                if (a < b) return false
+            const oldParts = compareVersion.split('.');
+            const newParts = currentVersion.split('.');
+
+            for (let i = 0; i < newParts.length; i++) {
+                const a = parseInt(newParts[i], 10);
+                const b = parseInt(oldParts[i], 10);
+
+                if (a > b) {
+                    return true;
+                }
+                if (a < b) {
+                    return false;
+                }
             }
-            return false
-        },
+            return false;
+        }
     }
 });
