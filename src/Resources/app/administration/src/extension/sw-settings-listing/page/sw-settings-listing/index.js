8cdec3ff32e5d2224bad338e0ca53dc386cdf270
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-03-27 10:03:43 +0100

    Adapt changes for info help text

--- src/Resources/app/administration/src/extension/sw-settings-listing/page/sw-settings-listing/index.js
+++ src/Resources/app/administration/src/extension/sw-settings-listing/page/sw-settings-listing/index.js
@@ -2,4 +2,21 @@ import template from './sw-settings-listing.html.twig';
 
 Shopware.Component.override('sw-settings-listing', {
     template,
+
+    computed: {
+        version() {
+            const currentVersion = Shopware.Context.app.config.version
+            const compareVersion = '6.6.9'
+
+            const oldParts = compareVersion.split('.')
+            const newParts = currentVersion.split('.')
+            for (var i = 0; i < newParts.length; i++) {
+                const a = ~~newParts[i]
+                const b = ~~oldParts[i]
+                if (a > b) return true
+                if (a < b) return false
+            }
+            return false
+        },
+    }
 });
