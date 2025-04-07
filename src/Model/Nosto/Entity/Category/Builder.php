a296d5a1074f14f5721ce8df04474b619622d4b0
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-04-07 11:41:44 +0200

    Correct the changes

--- src/Model/Nosto/Entity/Category/Builder.php
+++ src/Model/Nosto/Entity/Category/Builder.php
@@ -42,7 +42,7 @@ class Builder
         }
 
         $url = '';
-        if ($category->getSeoUrls()?->getElements()) {
+        if ($category->getSeoUrls()->getElements() && $domain) {
             $lastSeoUrl = array_key_last($category->getSeoUrls()->getElements());
             $url = $category->getSeoUrls()->getElements()[$lastSeoUrl]->getSeoPathInfo();
             $nostoCategory->setUrl($domain->getUrl() . '/' . $url);
