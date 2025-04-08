9762377acca5fec569d8db9e571c3a3873f37657
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-04-08 11:14:07 +0200

    Implement Shopware Category Integration

---
+++ src/Model/Nosto/Entity/Category/Builder.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nosto\NostoIntegration\Model\Nosto\Entity\Category;
+
+use Nosto\Model\Category\Category as NostoCategory;
+use Nosto\NostoIntegration\Model\ConfigProvider;
+use Nosto\NostoIntegration\Model\Nosto\Entity\Category\Event\NostoCategoryBuiltEvent;
+use Shopware\Core\Content\Category\CategoryEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class Builder
+{
+    public function __construct(
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly SystemConfigService $systemConfigService,
+        private readonly ConfigProvider $configProvider,
+    ) {
+    }
+
+    public function build(
+        CategoryEntity $category,
+        SalesChannelContext $context,
+    ): NostoCategory {
+        $nostoCategory = new NostoCategory();
+        $nostoCategory->setId($category->getId());
+        $nostoCategory->setTitle($category->getName());
+        $nostoCategory->setAvailable($category->getActive());
+        $nostoCategory->setParentId($category->getParentId());
+
+        $domain = null;
+        if ($domains = $context->getSalesChannel()->getDomains()) {
+            $domainId = (string) $this->configProvider->getDomainId(
+                $context->getSalesChannelId(),
+                $context->getLanguageId(),
+            );
+
+            $domain = $domains->has($domainId) ? $domains->get($domainId) : $domains->first();
+        }
+
+        $url = '';
+        if ($category->getSeoUrls()->getElements() && $domain) {
+            $lastSeoUrl = array_key_last($category->getSeoUrls()->getElements());
+            $url = $category->getSeoUrls()->getElements()[$lastSeoUrl]->getSeoPathInfo();
+            $nostoCategory->setUrl($domain->getUrl() . '/' . $url);
+        }
+
+        if ($url !== '') {
+            $nostoCategory->setPath('/' . $url);
+            $nostoCategory->setCategoryString('/' . $url);
+        } else {
+            $nostoCategory->setPath('');
+            $nostoCategory->setCategoryString('');
+        }
+
+        $this->eventDispatcher->dispatch(
+            new NostoCategoryBuiltEvent($category, $nostoCategory, $context->getContext()),
+        );
+        return $nostoCategory;
+    }
+}
