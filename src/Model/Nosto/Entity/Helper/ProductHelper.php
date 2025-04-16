a0b2b633e8582cd20887f346066f56ec2f830382
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-04-16 11:35:57 +0200

    Add visibility custom field

--- src/Model/Nosto/Entity/Helper/ProductHelper.php
+++ src/Model/Nosto/Entity/Helper/ProductHelper.php
@@ -79,6 +79,7 @@ class ProductHelper
         $criteria->addAssociation('manufacturer');
         $criteria->addAssociation('manufacturer.media');
         $criteria->addAssociation('categoriesRo');
+        $criteria->addAssociation('visibilities');
 
         return $criteria;
     }
