6f255f2fece4271642547c64e4933d84657d373f
Radomir Butacevic <radomir.butacevic@soprex.com>
2025-04-24 15:47:43 +0200

    Correct the changes

--- src/Model/Nosto/Entity/Product/SkuBuilder.php
+++ src/Model/Nosto/Entity/Product/SkuBuilder.php
@@ -10,6 +10,7 @@ use Nosto\NostoIntegration\Enums\StockFieldOptions;
 use Nosto\NostoIntegration\Model\ConfigProvider;
 use Nosto\NostoIntegration\Model\Nosto\Entity\Helper\ProductHelper;
 use Nosto\Types\Product\ProductInterface;
+use Shopware\Core\Content\Product\Aggregate\ProductVisibility\ProductVisibilityDefinition;
 use Shopware\Core\Content\Product\ProductEntity;
 use Shopware\Core\Defaults;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
