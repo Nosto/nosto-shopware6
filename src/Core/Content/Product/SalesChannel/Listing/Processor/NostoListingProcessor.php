b6b58fc2ee7379365cfb9d130343c6b4d8e6c26d
Ugljesa Zivaljevic <ugljesa.zivaljevic@soprex.com>
2025-05-15 09:23:49 +0200

    Adapt Processor execution

--- src/Core/Content/Product/SalesChannel/Listing/Processor/NostoListingProcessor.php
+++ src/Core/Content/Product/SalesChannel/Listing/Processor/NostoListingProcessor.php
@@ -28,6 +28,8 @@ class NostoListingProcessor extends AbstractListingProcessor
 
     public function prepare(Request $request, Criteria $criteria, SalesChannelContext $context): void
     {
+        // TODO: Remove
+        echo 'ProcessNosto' . '<br>';
         if (SearchHelper::shouldHandleRequest(
             $context,
             $this->configProvider,
