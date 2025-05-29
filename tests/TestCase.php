3383652b399f0f6cf856441bd00d569c2bfce482
Radomir98 <radomir.butacevic@soprex.com>
2025-05-29 12:57:27 +0200

    Test commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,23 +4,8 @@ declare(strict_types=1);
 
 namespace Nosto\NostoIntegration\Tests;
 
-use PHPUnit\Framework\TestCase as PhpUnitTestCase;
-use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
-use Shopware\Core\Kernel;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestCase;
 
-class TestCase extends PhpUnitTestCase
+class TestCase extends IntegrationTestCase
 {
-    use IntegrationTestBehaviour;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        Kernel::getConnection()->beginTransaction();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-        Kernel::getConnection()->rollBack();
-    }
 }
