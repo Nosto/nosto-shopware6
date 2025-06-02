b19a48ba4e7f1732c0f518359612bf66143a09f2
Lisa <47558295+lisafindo@users.noreply.github.com>
2025-06-02 13:31:52 +0200

    change to Testcase

--- tests/NostoIntegrationTest.php
+++ tests/NostoIntegrationTest.php
@@ -7,14 +7,14 @@ namespace Nosto\NostoIntegration\Tests;
 use Nosto\NostoIntegration\NostoIntegration;
 use Nosto\NostoIntegration\Utils\MigrationHelper;
 use Nosto\Scheduler\NostoScheduler;
+use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\Plugin\Context\ActivateContext;
 use Shopware\Core\Framework\Plugin\Util\AssetService;
 use Shopware\Core\Framework\Test\TestCaseBase\DatabaseTransactionBehaviour;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
-use Shopware\Core\Framework\Test\TestCaseBase\KernelTestCase;
 
-class NostoIntegrationTest extends KernelTestCase
+class NostoIntegrationTest extends TestCase
 {
     use IntegrationTestBehaviour;
     use DatabaseTransactionBehaviour;
