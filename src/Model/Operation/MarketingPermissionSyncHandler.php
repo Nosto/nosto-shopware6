1fd0318f9032cf09d3fe0f554fa8c71ab8450e5e
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2025-06-04 21:14:33 +0300

    fixing linter

--- src/Model/Operation/MarketingPermissionSyncHandler.php
+++ src/Model/Operation/MarketingPermissionSyncHandler.php
@@ -12,9 +12,9 @@ use Nosto\Scheduler\Model\Job\{JobHandlerInterface, JobResult};
 use Nosto\Types\Signup\AccountInterface;
 use Shopware\Core\Content\Newsletter\SalesChannel\NewsletterSubscribeRoute;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\{EntityCollection, EntityRepository};
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\{EntityCollection, EntityRepository};
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Throwable;
 
