4ec931ffe0689a6326f8fec58c925125e5097f44
Radomir98 <radomir.butacevic@soprex.com>
2025-06-06 15:56:06 +0200

    Adapt PHPLint test

--- src/Search/Request/Handler/NavigationRequestHandler.php
+++ src/Search/Request/Handler/NavigationRequestHandler.php
@@ -11,8 +11,8 @@ use Nosto\NostoIntegration\Model\Nosto\Entity\Product\Builder;
 use Nosto\NostoIntegration\Model\Nosto\Entity\Product\Category\TreeBuilder;
 use Nosto\Result\Graphql\Search\SearchResult;
 use Shopware\Core\Content\Category\CategoryEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\Request;
 
