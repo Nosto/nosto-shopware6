5e119aa8f200e41a007f6abf76bf46612bb6134a
Radomir98 <radomir.butacevic@soprex.com>
2025-07-28 13:09:07 +0200

    Add AsyncMessageInterface to CategorySyncMessage to support async message handling

--- src/Async/CategorySyncMessage.php
+++ src/Async/CategorySyncMessage.php
@@ -7,8 +7,9 @@ namespace Nosto\NostoIntegration\Async;
 use Nosto\NostoIntegration\Model\Operation\CategorySyncHandler;
 use Nosto\Scheduler\Async\ParentAwareMessageInterface;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\MessageQueue\AsyncMessageInterface;
 
-class CategorySyncMessage extends AbstractMessage implements ParentAwareMessageInterface
+class CategorySyncMessage extends AbstractMessage implements ParentAwareMessageInterface, AsyncMessageInterface
 {
     protected static string $defaultName = 'Category Sync Operation';
 
