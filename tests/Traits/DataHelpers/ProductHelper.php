ff178228a7810814f0b182c2748105746b1d8b12
Radomir98 <radomir.butacevic@soprex.com>
2025-08-13 09:37:59 +0200

    Add product and category helper

---
+++ tests/Traits/DataHelpers/ProductHelper.php
@@ -0,0 +1,403 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nosto\NostoIntegration\Tests\Traits\DataHelpers;
+
+use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+trait ProductHelper
+{
+    use IntegrationTestBehaviour;
+    use CategoryHelper;
+
+    private SalesChannelContext $salesChannelContext;
+
+    public const PRODUCT_ASSOCIATIONS = [
+        'seoUrls',
+        'translations',
+        'searchKeywords',
+        'media',
+        'manufacturer',
+        'manufacturer.translations',
+        'cover',
+        'variantListingConfig',
+        'visibilities',
+    ];
+
+    public const VARIANT_ASSOCIATIONS = [
+        'seoUrls',
+        'media',
+        'categories',
+        'categories.seoUrls',
+        'options',
+        'options.group',
+        'properties',
+        'properties.group',
+    ];
+
+    public function createTestProduct(
+        array $overrideData = [],
+        bool $withVariant = false,
+        bool $overrideRecursively = false,
+        bool $withManufacturer = true
+    ): ?ProductEntity {
+        $mainProduct = $this->buildProductInfo($overrideData, $overrideRecursively, $withManufacturer);
+        $products = [$mainProduct];
+
+        if ($withVariant) {
+            $products[] = [
+                'id' => 'a5a1c99e6fbf2316523151de9e1aad31',
+                'productNumber' => 'NOSTO001.1',
+                'ean' => 'NS0011',
+                'manufacturerNumber' => 'MAN0011',
+                'name' => 'NOSTO VARIANT 1',
+                'stock' => 10,
+                'active' => true,
+                'parentId' => $mainProduct['id'],
+                'tax' => ['name' => '9%', 'taxRate' => 9],
+                'price' => [['currencyId' => Defaults::CURRENCY, 'gross' => 15, 'net' => 10, 'linked' => false]]
+            ];
+
+            $products[] = [
+                'id' => 'edc0f84ed1e20dedff0ce81c1838758a',
+                'productNumber' => 'NOSTO001.2',
+                'ean' => 'NS0012',
+                'manufacturerNumber' => 'MAN0012',
+                'name' => 'NOSTO VARIANT 2',
+                'stock' => 7,
+                'active' => true,
+                'parentId' => $mainProduct['id'],
+                'tax' => ['name' => '9%', 'taxRate' => 9],
+                'price' => [['currencyId' => Defaults::CURRENCY, 'gross' => 80, 'net' => 66, 'linked' => false]]
+            ];
+        }
+
+        return $this->upsertProducts($products);
+    }
+
+    public function createProductWithMultipleVariants(
+        ?string $parentId = null,
+        ?string $expectedFirstVariantId = null,
+        ?string $expectedSecondVariantId = null,
+        ?string $expectedThirdVariantId = null
+    ): ?ProductEntity {
+        $firstOptionId = Uuid::randomHex();
+        $secondOptionId = Uuid::randomHex();
+        $thirdOptionId = Uuid::randomHex();
+        $optionGroupId = Uuid::randomHex();
+
+        $parentId ??= Uuid::randomHex();
+
+        $variants = [];
+        $variants[] = $this->getBasicVariantData([
+            'id' => $expectedFirstVariantId ?? Uuid::randomHex(),
+            'parentId' => $parentId,
+            'productNumber' => 'NOSTO001.1',
+            'name' => 'NOSTO VARIANT 1',
+            'options' => [
+                ['id' => $firstOptionId]
+            ],
+        ]);
+
+        $variants[] = $this->getBasicVariantData([
+            'id' => $expectedSecondVariantId ?? Uuid::randomHex(),
+            'parentId' => $parentId,
+            'productNumber' => 'NOSTO001.2',
+            'name' => 'NOSTO VARIANT 2',
+            'options' => [
+                ['id' => $secondOptionId]
+            ],
+        ]);
+
+        $variants[] = $this->getBasicVariantData([
+            'id' => $expectedThirdVariantId ?? Uuid::randomHex(),
+            'parentId' => $parentId,
+            'productNumber' => 'NOSTO001.3',
+            'name' => 'NOSTO VARIANT 3',
+            'options' => [
+                ['id' => $thirdOptionId]
+            ],
+        ]);
+
+        return $this->createVisibleTestProductWithCustomVariants([
+            'id' => $parentId,
+            'active' => false,
+            'configuratorSettings' => [
+                [
+                    'option' => [
+                        'id' => $firstOptionId,
+                        'name' => 'Red',
+                        'group' => [
+                            'id' => $optionGroupId,
+                            'name' => 'Color',
+                        ],
+                    ],
+                ],
+                [
+                    'option' => [
+                        'id' => $secondOptionId,
+                        'name' => 'Orange',
+                        'group' => [
+                            'id' => $optionGroupId,
+                            'name' => 'Color',
+                        ],
+                    ],
+                ],
+                [
+                    'option' => [
+                        'id' => $thirdOptionId,
+                        'name' => 'Green',
+                        'group' => [
+                            'id' => $optionGroupId,
+                            'name' => 'Color',
+                        ],
+                    ],
+                ],
+            ]
+        ], $variants);
+    }
+
+    public function createVisibleTestProductWithCustomVariants(
+        array $overrides = [],
+        array $variants = []
+    ): ?ProductEntity {
+        $mainProduct = $this->buildProductInfo(
+            array_merge($this->getBasicVisibilityData(), $overrides)
+        );
+        $products = [$mainProduct];
+
+        foreach ($variants as $variant) {
+            $variant['parentId'] = $mainProduct['id'];
+            $products[] = $this->buildProductInfo(
+                array_merge($this->getBasicVisibilityData(), $variant)
+            );
+        }
+
+        return $this->upsertProducts($products);
+    }
+
+    public function getBasicVisibilityData(): array
+    {
+        return [
+            'visibilities' => [
+                [
+                    'id' => Uuid::randomHex(),
+                    'salesChannelId' => Defaults::SALES_CHANNEL_TYPE_STOREFRONT,
+                    'visibility' => 20
+                ]
+            ]
+        ];
+    }
+
+    public function getBasicVariantData(array $overrides = []): array
+    {
+        return array_merge([
+            'stock' => 10,
+            'active' => true,
+            'tax' => ['name' => '9%', 'taxRate' => 9],
+            'price' => [['currencyId' => Defaults::CURRENCY, 'gross' => 15, 'net' => 10, 'linked' => false]]
+        ], $overrides);
+    }
+
+    public function upsertProducts(array $productInfo): ?ProductEntity
+    {
+        $context = Context::createDefaultContext();
+        $this->getContainer()->get('product.repository')->upsert($productInfo, $context);
+
+        $id = current($productInfo)['id'];
+        try {
+            $criteria = new Criteria([$id]);
+            $criteria->addAssociations(self::PRODUCT_ASSOCIATIONS);
+            $criteria->addAssociations(self::VARIANT_ASSOCIATIONS);
+
+            return $this->getContainer()->get('product.repository')->search($criteria, $context)->first();
+        } catch (InconsistentCriteriaIdsException $e) {
+            return null;
+        }
+    }
+
+    public function buildProductInfo(
+        array $overrideData = [],
+        bool $overrideRecursively = false,
+        bool $withManufacturer = true
+    ): array {
+        $id = Uuid::randomHex();
+        $redId = Uuid::randomHex();
+        $colorId = Uuid::randomHex();
+
+        $container = $this->getContainer();
+
+        $seoUrlRepo = $container->get('seo_url.repository');
+        $seoUrls = $seoUrlRepo->search(new Criteria(), Context::createDefaultContext());
+        $seoUrlsStoreFrontContext = $seoUrlRepo->search(new Criteria(), $this->salesChannelContext->getContext());
+
+        $productSeoUrls = [];
+        if ($seoUrls->count() === 0 && $seoUrlsStoreFrontContext->count() === 0) {
+            $productSeoUrls = [
+                [
+                    'pathInfo' => '/detail/' . $id,
+                    'seoPathInfo' => 'Awesome-Seo-Url/&ecause/SÄÖ/is/$mportant+',
+                    'isCanonical' => true,
+                    'routeName' => 'frontend.detail.page'
+                ],
+            ];
+        }
+        $productData = [];
+        $productData = array_merge(
+            $productData,
+            $this->getNameValues($overrideData['name'] ?? 'NOSTO Product')
+        );
+
+        $productData = array_merge($productData, [
+            'id' => $id,
+            'productNumber' => 'NOSTO001',
+            'stock' => 10,
+            'ean' => 'NS001',
+            'description' => 'NOSTO Description',
+            'tags' => [
+                ['id' => Uuid::randomHex(), 'name' => 'NOSTO Tag']
+            ],
+            'cover' => $this->getDefaultCoverData(),
+            'price' => [['currencyId' => Defaults::CURRENCY, 'gross' => 15, 'net' => 10, 'linked' => false]],
+            'tax' => [
+                'id' => $this->salesChannelContext->getTaxRules()->first()?->getId() ?? Uuid::randomHex(),
+                'name' => '9%',
+                'taxRate' => 9
+            ],
+            'categories' => $this->getDefaultCategories(),
+            'seoUrls' => $productSeoUrls,
+            'properties' => [
+                [
+                    'id' => $redId,
+                    'name' => 'red',
+                    'group' => ['id' => $colorId, 'name' => 'color'],
+                ]
+            ],
+            'options' => [
+                [
+                    'id' => $redId,
+                    'name' => 'red',
+                    'group' => ['id' => $colorId, 'name' => $colorId],
+                ]
+            ],
+            'configuratorSettings' => [
+                [
+                    'id' => $redId,
+                    'price' => ['currencyId' => Defaults::CURRENCY, 'gross' => 50, 'net' => 25, 'linked' => false],
+                    'option' => [
+                        'id' => $redId,
+                        'name' => 'red',
+                        'group' => ['id' => $colorId, 'name' => $colorId],
+                    ],
+                ]
+            ],
+            'visibilities' => [
+                [
+                    'productId' => $id,
+                    'salesChannelId' => $this->salesChannelContext->getSalesChannelId(),
+                    'visibility' => 30
+                ]
+            ]
+        ]);
+
+        if ($withManufacturer) {
+            $productData = array_merge($productData, [
+                'manufacturerNumber' => 'MAN001',
+                'manufacturer' => ['name' => 'NOSTO'],
+            ]);
+        }
+
+        return $overrideRecursively
+            ? array_replace_recursive($productData, $overrideData)
+            : array_merge($productData, $overrideData);
+    }
+
+    public function getDefaultCategories(): array
+    {
+        $categoryId = Uuid::randomHex();
+        $newCategoryId = Uuid::randomHex();
+        $navigationCategoryId = $this->salesChannelContext->getSalesChannel()->getNavigationCategoryId();
+
+        $this->createTestCategory([
+            'name' => 'NOSTO Main 2',
+            'children' => [
+                [
+                    'id' => $newCategoryId,
+                    'name' => 'NOSTO Sub',
+                    'children' => [
+                        [
+                            'id' => Uuid::randomHex(),
+                            'name' => 'Very deep'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+        return [
+            [
+                'parentId' => $navigationCategoryId ?: null,
+                'id' => $categoryId,
+                'name' => 'NOSTO Category',
+                'seoUrls' => [
+                    [
+                        'pathInfo' => 'navigation/' . $categoryId,
+                        'seoPathInfo' => 'Nosto-Category',
+                        'isCanonical' => true,
+                        'routeName' => 'frontend.navigation.page'
+                    ]
+                ]
+            ],
+            [
+                'parentId' => $newCategoryId,
+                'id' => Uuid::randomHex(),
+                'name' => 'NOSTO Sub of Sub'
+            ]
+        ];
+    }
+
+    public function getNameValues(string $name): array
+    {
+        return [
+            'name' => $name,
+            'translations' => [
+                'de-DE' => [
+                    'name' => $name . ' DE',
+                ],
+                'en-GB' => [
+                    'name' => $name . ' EN',
+                ],
+            ],
+        ];
+    }
+
+    public function getDefaultCoverData(): array
+    {
+        return [
+            'media' => [
+                'url' => 'https://via.placeholder.com/1000',
+                'private' => false,
+                'mediaType' => 'image/png',
+                'mimeType' => 'image/png',
+                'fileExtension' => 'png',
+                'fileName' => 'nosto',
+                'thumbnails' => [
+                    [
+                        'width' => 600,
+                        'height' => 600,
+                        'highDpi' => false,
+                        'url' => 'https://via.placeholder.com/600'
+                    ]
+                ]
+            ],
+        ];
+    }
+}
