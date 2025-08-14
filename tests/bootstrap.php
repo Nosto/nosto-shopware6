01da0d31c60736d4b479ed4bd6994ba41fe12c28
ugljesaspx <ugljesaspx@yourcompany.com>
2025-08-14 12:47:15 +0200

    Tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// Load plugin's own autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Load main project's autoloader
+require_once __DIR__ . '/../../../../vendor/autoload.php'; 
