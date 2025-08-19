d083feb8b6491b1052dd90fc4067abe037bc1c74
Parisa Rahimi <parisa.rahimi@nosto.com>
2025-08-19 16:31:16 +0300

    command ./vendor/bin/ecs check --fix ran

--- src/Service/NostoJobSyncService.php
+++ src/Service/NostoJobSyncService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Nosto\NostoIntegration\Service;
 
 use Doctrine\DBAL\Connection;
+use Exception;
 use Nosto\Scheduler\Entity\Job\JobEntity;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
@@ -12,7 +13,6 @@ use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\AndFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
-use Exception;
 
 class NostoJobSyncService
 {
@@ -27,22 +27,25 @@ class NostoJobSyncService
     {
         $jobIds = $this->getAllJobIds($context, $handlerCode);
         if (empty($jobIds)) {
-            throw new Exception('There are currently no jobs available for cancellation, all jobs are completed successfully.');
+            throw new Exception(
+                'There are currently no jobs available for cancellation, all jobs are completed successfully.',
+            );
         }
 
         try {
             $this->connection->executeStatement(
-                "DELETE FROM `messenger_messages` WHERE body LIKE '%nosto-integration%'"
+                "DELETE FROM `messenger_messages` WHERE body LIKE '%nosto-integration%'",
             );
 
-            $deleteIds = array_map(fn($id) => ['id' => $id], array_values($jobIds));
+            $deleteIds = array_map(fn ($id) => [
+                'id' => $id,
+            ], array_values($jobIds));
             $this->jobMessageRepository->delete($deleteIds, $context);
             $this->jobRepository->delete($deleteIds, $context);
         } catch (Exception $e) {
             throw new Exception('An error occurred while cancelling full product sync jobs');
         }
     }
-
 
     private function getAllJobIds(Context $context, string $handlerCode): array
     {
@@ -53,10 +56,10 @@ class NostoJobSyncService
                 new EqualsFilter('type', $handlerCode),
                 new EqualsFilter('parentId', null),
                 new EqualsAnyFilter('status', [JobEntity::TYPE_PENDING, JobEntity::TYPE_RUNNING]),
-            ])
+            ]),
         );
         $parentJobs = $this->jobRepository->search($parentCriteria, $context)->getEntities();
-        $parentJobIds = array_map(fn($job) => $job->getId(), $parentJobs->getElements());
+        $parentJobIds = array_map(fn ($job) => $job->getId(), $parentJobs->getElements());
 
         // 2. Find child jobs: parent_id IN (parentJobIds)
         $childJobIds = [];
@@ -66,10 +69,10 @@ class NostoJobSyncService
                 new AndFilter([
                     new EqualsFilter('type', $handlerCode),
                     new EqualsAnyFilter('parentId', $parentJobIds),
-                ])
+                ]),
             );
             $childJobs = $this->jobRepository->search($childCriteria, $context)->getEntities();
-            $childJobIds = array_map(fn($job) => $job->getId(), $childJobs->getElements());
+            $childJobIds = array_map(fn ($job) => $job->getId(), $childJobs->getElements());
         }
 
         // 3. Return union of parent and child job IDs
