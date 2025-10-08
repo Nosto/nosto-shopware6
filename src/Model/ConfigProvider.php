d7459a53a6f68a261286ce7277ec622bb7de0ac1
Lisa <47558295+lisafindo@users.noreply.github.com>
2025-10-08 09:53:11 -0400

    add typesafety

--- src/Model/ConfigProvider.php
+++ src/Model/ConfigProvider.php
@@ -361,7 +361,8 @@ class ConfigProvider
         return $this->configService->getConfigWithInheritance($channelId, $languageId);
     }
 
-    public function getBatchSize() {
-        return $this->configService->get(NostoConfigService::SYNC_BATCH_SIZE, null, null);
+    public function getBatchSize(): int
+    {
+        return $this->configService->getInt(NostoConfigService::SYNC_BATCH_SIZE, null, null);
     }
 }
