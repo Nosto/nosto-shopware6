3961d4fe6ee05d0427b3235b35c08cc19900e596
Parisa Rahimi <parisa.rahimi@nosto.com>
2026-02-04 14:41:43 +0200

    Rollback handle external filters in product listing and search routes

--- src/Decorator/Core/Content/Product/SalesChannel/Listing/ResolveCriteriaProductListingRoute.php
+++ src/Decorator/Core/Content/Product/SalesChannel/Listing/ResolveCriteriaProductListingRoute.php
@@ -58,16 +58,9 @@ class ResolveCriteriaProductListingRoute extends AbstractProductListingRoute
 
         $this->processor->prepare($request, $criteria, $context);
 
-        $filterCountBefore = \count($criteria->getFilters());
-        $postFilterCountBefore = \count($criteria->getPostFilters());
         $this->eventDispatcher->dispatch(
             new ProductListingCriteriaEvent($request, $criteria, $context),
         );
-        if (\count($criteria->getFilters()) > $filterCountBefore
-            || \count($criteria->getPostFilters()) > $postFilterCountBefore
-        ) {
-            $criteria->addState('nosto.external-filters');
-        }
 
         $response = $this->getDecorated()->load($categoryId, $request, $context, $criteria);
 
