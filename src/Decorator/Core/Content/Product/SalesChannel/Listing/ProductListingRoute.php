bb0716d28655b657ac5a8fad44c7ad072b38610f
Ilja Ganulevics <ilja.ganulevics@nosto.com>
2026-02-18 16:15:11 +0200

    incorrect criteria was used/overwritten

--- src/Decorator/Core/Content/Product/SalesChannel/Listing/ProductListingRoute.php
+++ src/Decorator/Core/Content/Product/SalesChannel/Listing/ProductListingRoute.php
@@ -85,9 +85,6 @@ class ProductListingRoute extends AbstractProductListingRoute
                 return $this->decorated->load($categoryId, $request, $context, $criteria);
             }
 
-            // Preserve post-filters from original criteria (added by other plugins via ProductListingCriteriaEvent)
-            $originalPostFilters = $criteria?->getPostFilters() ?? [];
-
             $criteria->addFilter(
                 new ProductAvailableFilter(
                     $context->getSalesChannel()->getId(),
@@ -95,24 +92,19 @@ class ProductListingRoute extends AbstractProductListingRoute
                 ),
             );
             /** @var CategoryEntity $category */
-            $criteria = NostoCriteriaFactory::createWithIds([$categoryId]);
-            $criteria->addAssociation('seoUrls');
+            $categoryCriteria = NostoCriteriaFactory::createWithIds([$categoryId]);
+            $categoryCriteria->addAssociation('seoUrls');
             $criteria->addAssociation('options.group');
 
             /** @var CategoryEntity $category */
             $category = $this->categoryRepository->search(
-                $criteria,
+                $categoryCriteria,
                 $context->getContext(),
             )->first();
 
             $streamId = $this->extendCriteria($context, $criteria, $category);
 
             $this->listingProcessor->prepare($request, $criteria, $context);
-
-            // Re-apply post-filters from original criteria to ensure compatibility with other plugins
-            foreach ($originalPostFilters as $postFilter) {
-                $criteria->addPostFilter($postFilter);
-            }
 
             $productListing = ProductListingResult::createFrom(
                 $this->fetchProductsById($criteria, $context),
