71cf29136e55a1f471166671f1ee70c7c6a08e60
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-03-30 08:53:36 +0300

    ADS-5342 fix PHPUnit launcher

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -3,5 +3,10 @@
 # Script should be run with "composer test"
 
 dir=`pwd`
+phpunit_bin="$dir/../../../vendor/bin/phpunit"
 
-"$dir/vendor/bin/phpunit" --configuration="$dir/phpunit.xml.dist" --colors=always "$@"
+if [ ! -x "$phpunit_bin" ]; then
+    phpunit_bin="$dir/vendor/bin/phpunit"
+fi
+
+exec "$phpunit_bin" --configuration="$dir/phpunit.xml.dist" --colors=always "$@"
