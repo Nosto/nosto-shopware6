8c01fd1cd6f9d91a738e9846b63b9b58087fbc2d
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-03-31 09:40:32 +0300

    ADS-5342 update final summary for each version

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -15,7 +15,18 @@ fi
 phpunit_args=("$@")
 
 if [ -n "$CI" ]; then
-    phpunit_args=("--no-output" "${phpunit_args[@]}")
+    phpunit_args=("--no-progress" "${phpunit_args[@]}")
 fi
 
 "$phpunit_bin" --configuration="$dir/phpunit.xml.dist" --colors=always "${phpunit_args[@]}"
+phpunit_exit_code=$?
+
+if [ -n "$CI" ]; then
+    if [ "$phpunit_exit_code" -eq 0 ]; then
+        printf '\033[32mPHPUnit passed\033[0m\n'
+    else
+        printf '\033[31mPHPUnit failed\033[0m\n'
+    fi
+fi
+
+exit "$phpunit_exit_code"
