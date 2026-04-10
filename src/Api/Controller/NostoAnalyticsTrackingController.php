350c1fa6db1ef6dccac8420853e302d1eed6b47d
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-04-10 20:59:12 +0300

    ADS-5341 fix pr comments

--- src/Api/Controller/NostoAnalyticsTrackingController.php
+++ src/Api/Controller/NostoAnalyticsTrackingController.php
@@ -131,7 +131,7 @@ class NostoAnalyticsTrackingController extends AbstractController
                     $data['isOrganic'] ?? true,
                     $data['isAutoCorrect'] ?? true,
                     $data['isAutoComplete'] ?? false,
-                    $data['searchType'] ?? (($data['isKeyword'] ?? false) ? 'keyword' : 'vector'),
+                    ($data['searchType'] ?? 'keyword') === 'keyword',
                     $data['isSorted'] ?? true,
                     $data['hasResults'] ?? true,
                     $data['isRefined'] ?? false,
--- src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
+++ src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
@@ -209,7 +209,6 @@ class ProductSearchRoute extends AbstractProductSearchRoute
             );
             $page = $productListing->getPage();
             $resultId = vsprintf('%s%s%s%s-%s%s-%s%s-%s%s-%s%s%s%s%s%s', str_split(Uuid::randomHex(), 2));
-            $searchType = $request->attributes->get('nostoSearchType') ?: 'keyword';
             $metadata = new AnalyticsSearchMetadataForGraphql(
                 $request->get('search') ?? null,
                 $resultId,
@@ -219,8 +218,8 @@ class ProductSearchRoute extends AbstractProductSearchRoute
                 true,
                 //autoComplete
                 false,
-                //searchType
-                $searchType,
+                //keyword
+                ($request->attributes->get('nostoSearchType') ?: 'keyword') === 'keyword',
                 //sorted
                 $request->get('order') != null,
                 //hasResults
@@ -233,7 +232,7 @@ class ProductSearchRoute extends AbstractProductSearchRoute
                 $productListing->getExtensions(),
                 [
                     'nosto_result_id' => $resultId,
-                    'nosto_search_type' => $searchType,
+                    'nosto_search_type' => $request->attributes->get('nostoSearchType') ?: 'keyword',
                 ],
             ));
             $tracker->impression($metadata, $productIds, $page, SearchHelper::getABTestsFromCookie($request));
