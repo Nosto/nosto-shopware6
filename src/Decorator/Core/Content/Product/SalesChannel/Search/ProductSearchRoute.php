dd0e79bfa7d5df6fce28591701b4a84e5be53b68
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-04-10 10:47:21 +0300

    ADS-5341 Send to search analytics search type v3

--- src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
+++ src/Decorator/Core/Content/Product/SalesChannel/Search/ProductSearchRoute.php
@@ -209,6 +209,7 @@ class ProductSearchRoute extends AbstractProductSearchRoute
             );
             $page = $productListing->getPage();
             $resultId = vsprintf('%s%s%s%s-%s%s-%s%s-%s%s-%s%s%s%s%s%s', str_split(Uuid::randomHex(), 2));
+            $searchType = $request->attributes->get('nostoSearchType') ?: 'keyword';
             $metadata = new AnalyticsSearchMetadataForGraphql(
                 $request->get('search') ?? null,
                 $resultId,
@@ -218,8 +219,8 @@ class ProductSearchRoute extends AbstractProductSearchRoute
                 true,
                 //autoComplete
                 false,
-                //keyword
-                false,
+                //searchType
+                $searchType,
                 //sorted
                 $request->get('order') != null,
                 //hasResults
@@ -228,9 +229,13 @@ class ProductSearchRoute extends AbstractProductSearchRoute
                 false,
             );
             //we need to know about the resultId that was used in the impression for the click analytic
-            $productListing->setExtensions([
-                "nosto_result_id" => $resultId,
-            ]);
+            $productListing->setExtensions(array_merge(
+                $productListing->getExtensions(),
+                [
+                    'nosto_result_id' => $resultId,
+                    'nosto_search_type' => $searchType,
+                ],
+            ));
             $tracker->impression($metadata, $productIds, $page, SearchHelper::getABTestsFromCookie($request));
         } catch (\Exception $e) {
             //@ToDo maybe send the the error to the nosto
