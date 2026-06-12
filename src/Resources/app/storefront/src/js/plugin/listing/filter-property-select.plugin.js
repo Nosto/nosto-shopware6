7358aada3c062d8e7c02658e2ca1b287bfa954d4
Parisa Rahimi <parisa.rahimi@nosto.com>
2026-06-12 11:50:11 +0300

    Make filter value counts updated after each filter

--- src/Resources/app/storefront/src/js/plugin/listing/filter-property-select.plugin.js
+++ src/Resources/app/storefront/src/js/plugin/listing/filter-property-select.plugin.js
@@ -12,6 +12,7 @@ export default class NostoFilterPropertySelectPlugin extends FilterPropertySelec
         const entities = properties.entities;
 
         if (!entities || !entities.length) {
+            this._refreshOptionCounts(activeItems);
             this.disableFilter();
             return;
         }
@@ -20,6 +21,7 @@ export default class NostoFilterPropertySelectPlugin extends FilterPropertySelec
         if (property) {
             activeItems.push(...property.options);
         } else {
+            this._refreshOptionCounts(activeItems);
             this.disableFilter();
             return;
         }
@@ -27,12 +29,44 @@ export default class NostoFilterPropertySelectPlugin extends FilterPropertySelec
         const actualValues = this.getValues();
 
         if (activeItems.length < 1 && actualValues[this.options.name].length === 0) {
+            this._refreshOptionCounts(activeItems);
             this.disableFilter();
             return;
         }
         this.enableFilter();
 
-
+        this._refreshOptionCounts(activeItems);
         this._disableInactiveFilterOptions(activeItems.map(entity => entity.id));
+    }
+
+    /**
+     * Updates the result count rendered next to each filter option with the
+     * counts of the current filter response. Options missing from the
+     * response have no matching products, so their count is set to 0.
+     *
+     * @private
+     */
+    _refreshOptionCounts(activeItems) {
+        const counts = {};
+        activeItems.forEach(item => {
+            if (typeof item.count === 'number') {
+                counts[item.id] = item.count;
+            }
+        });
+
+        const checkboxes = this.el.querySelectorAll(this.options.checkboxSelector);
+        checkboxes.forEach(checkbox => {
+            const countEl = this.el.querySelector(
+                `label[for="${CSS.escape(checkbox.id)}"] .filter-option-count`,
+            );
+            if (!countEl) {
+                return;
+            }
+
+            const count = Object.prototype.hasOwnProperty.call(counts, checkbox.id)
+                ? counts[checkbox.id]
+                : 0;
+            countEl.textContent = `(${count})`;
+        });
     }
 }
