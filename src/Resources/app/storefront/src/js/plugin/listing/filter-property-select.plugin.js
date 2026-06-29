aaf52aee6a0166e6d14505d6a3819bba0f1de38f
Parisa Rahimi <parisa.rahimi@nosto.com>
2026-06-29 11:33:38 +0300

    Update checkbox value to be raw valueId as expected by backend

--- src/Resources/app/storefront/src/js/plugin/listing/filter-property-select.plugin.js
+++ src/Resources/app/storefront/src/js/plugin/listing/filter-property-select.plugin.js
@@ -1,6 +1,88 @@
 import PluginManager from 'src/plugin-system/plugin.manager';
+import DomAccess from 'src/helper/dom-access.helper';
+import Iterator from 'src/helper/iterator.helper';
 
 export default class NostoFilterPropertySelectPlugin extends PluginManager.getPlugin('FilterPropertySelect').get('class') {
+    getValues() {
+        const checkedCheckboxes =
+            DomAccess.querySelectorAll(this.el, `${this.options.checkboxSelector}:checked`, false);
+
+        const selection = [];
+
+        if (checkedCheckboxes) {
+            Iterator.iterate(checkedCheckboxes, (checkbox) => {
+                selection.push(checkbox.value);
+            });
+        }
+
+        this.selection = selection;
+        this._updateCount();
+
+        const values = {};
+        values[this.options.name] = selection;
+
+        return values;
+    }
+
+    getLabels() {
+        const activeCheckboxes =
+            DomAccess.querySelectorAll(this.el, `${this.options.checkboxSelector}:checked`, false);
+
+        const labels = [];
+
+        if (activeCheckboxes) {
+            Iterator.iterate(activeCheckboxes, (checkbox) => {
+                labels.push({
+                    label: checkbox.dataset.label,
+                    id: checkbox.id,
+                });
+            });
+        }
+
+        return labels;
+    }
+
+    setValuesFromUrl(params = {}) {
+        let stateChanged = false;
+
+        const properties = params[this.options.name];
+        const ids = properties ? properties.split('|') : [];
+
+        const uncheckItems = this.selection.filter(x => !ids.includes(x));
+        const checkItems = ids.filter(x => !this.selection.includes(x));
+
+        if (uncheckItems.length > 0 || checkItems.length > 0) {
+            stateChanged = true;
+        }
+
+        checkItems.forEach(id => {
+            const checkboxEl = this._getCheckboxByIdOrValue(id);
+
+            if (checkboxEl) {
+                checkboxEl.checked = true;
+                this.selection.push(checkboxEl.value);
+            }
+        });
+
+        uncheckItems.forEach(id => {
+            this.reset(id);
+
+            this.selection = this.selection.filter(item => item !== id);
+        });
+
+        this._updateCount();
+
+        return stateChanged;
+    }
+
+    reset(id) {
+        const checkboxEl = this._getCheckboxByIdOrValue(id);
+
+        if (checkboxEl) {
+            checkboxEl.checked = false;
+        }
+    }
+
     refreshDisabledState(filter) {
         // Prevent disabling if propertyName is not set correctly
         if (this.options.propertyName === '') {
@@ -33,6 +115,32 @@ export default class NostoFilterPropertySelectPlugin extends PluginManager.getPl
         this.enableFilter();
 
 
-        this._disableInactiveFilterOptions(activeItems.map(entity => `${this.options.name}-${entity.id}`));
+        this._disableInactiveFilterOptions(activeItems.map(entity => entity.id));
+    }
+
+    _disableInactiveFilterOptions(activeItemIds) {
+        const checkboxes = DomAccess.querySelectorAll(this.el, this.options.checkboxSelector);
+
+        Iterator.iterate(checkboxes, (checkbox) => {
+            if (checkbox.checked === true) {
+                return;
+            }
+
+            if (activeItemIds.includes(checkbox.value)) {
+                this.enableOption(checkbox);
+            } else {
+                this.disableOption(checkbox);
+            }
+        });
+    }
+
+    _getCheckboxByIdOrValue(id) {
+        const checkboxes = DomAccess.querySelectorAll(this.el, this.options.checkboxSelector, false);
+
+        if (!checkboxes) {
+            return null;
+        }
+
+        return Array.from(checkboxes).find(checkbox => checkbox.id === id || checkbox.value === id);
     }
 }
