03ea7395c1e78dd98d73a1fbfe4f84aa58f9108a
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-07-17 10:14:02 +0300

    ADS-5462 resolve pr comments

--- src/Resources/app/storefront/src/js/plugin/nosto-configuration.plugin.js
+++ src/Resources/app/storefront/src/js/plugin/nosto-configuration.plugin.js
@@ -5,7 +5,10 @@ import CookieStorage from 'src/helper/storage/cookie-storage.helper';
 import { COOKIE_CONFIGURATION_UPDATE } from 'src/plugin/cookie/cookie-configuration.plugin';
 import CookiePermissionPlugin from 'src/plugin/cookie/cookie-permission.plugin';
 
-export const NOSTO_COOKIE_KEY = 'nosto-integration-track-allow';
+export const NOSTO_COOKIE_KEY = 'nosto-integration-allowed';
+
+// Former cookie name; still honored so already-consented shoppers keep working.
+export const LEGACY_NOSTO_COOKIE_KEY = 'nosto-integration-track-allow';
 
 export default class NostoConfiguration extends window.PluginBaseClass {
     static options = {
@@ -22,6 +25,10 @@ export default class NostoConfiguration extends window.PluginBaseClass {
         this.watchCookieConsent();
     }
 
+    _hasConsentCookie() {
+        return CookieStorage.getItem(NOSTO_COOKIE_KEY) || CookieStorage.getItem(LEGACY_NOSTO_COOKIE_KEY);
+    }
+
     _registerInitializationEvents() {
         window.addEventListener('scroll', this._prepareForInitialization.bind(this), {once: true});
     }
@@ -32,7 +39,7 @@ export default class NostoConfiguration extends window.PluginBaseClass {
     }
 
     _initNosto() {
-        if (CookieStorage.getItem(NOSTO_COOKIE_KEY)) {
+        if (this._hasConsentCookie()) {
             this.storage = Storage;
 
             if (this.options.initializeAfter) {
@@ -137,7 +144,7 @@ export default class NostoConfiguration extends window.PluginBaseClass {
     }
 
     watchCookieConsent() {
-        if (CookieStorage.getItem(NOSTO_COOKIE_KEY)) {
+        if (this._hasConsentCookie()) {
             return;
         }
 
@@ -146,7 +153,7 @@ export default class NostoConfiguration extends window.PluginBaseClass {
         }
 
         this._cookieWatcher = window.setInterval(() => {
-            if (!CookieStorage.getItem(NOSTO_COOKIE_KEY)) {
+            if (!this._hasConsentCookie()) {
                 return;
             }
 
