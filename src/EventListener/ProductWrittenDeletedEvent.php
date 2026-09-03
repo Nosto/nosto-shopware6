83d4e3ddf36c1fea1dc3c65e1ec5e27dabe926ac
Lisa <47558295+lisafindo@users.noreply.github.com>
2026-09-03 11:40:42 +0200

    add vary parameter for cookie

--- src/EventListener/ProductWrittenDeletedEvent.php
+++ src/EventListener/ProductWrittenDeletedEvent.php
@@ -70,6 +70,9 @@ class ProductWrittenDeletedEvent implements EventSubscriberInterface
                 $response->setMaxAge($cacheTtlSeconds);
                 $response->setSharedMaxAge($cacheTtlSeconds);
                 $response->setStaleWhileRevalidate($cacheTtlSeconds);
+                // Category results can vary by Nosto's visitor and experiment cookies. Ensure
+                // that shared HTTP caches keep a separate representation for each cookie header.
+                $response->setVary(['Cookie'], false);
                 $expiryTime = new \DateTimeImmutable(sprintf('+%d seconds', $cacheTtlSeconds));
                 $response->setExpires($expiryTime);
             } else {
